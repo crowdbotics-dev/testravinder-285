a4a87cd1512d10a546ec1ccec57e77c422cc2126
ravinder yadav <ravinder.y@crowdbotics.com>
2024-04-17 13:36:41 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "testravinder-285",
-  url: "https://testravinder-285.botics.co",
-  api: "https://testravinder-285.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "testravinder-285",
+  "url": "https://testravinder-285.botics.co",
+  "api": "https://testravinder-285.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
